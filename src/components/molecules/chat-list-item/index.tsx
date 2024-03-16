f7194395dfd79ce43bc79a6407d4666749acbfc0
Daniyar <denaeshev@gmail.com>
2024-03-16 13:48:35 +0300

    change radius in chat item list

--- src/components/molecules/chat-list-item/index.tsx
+++ src/components/molecules/chat-list-item/index.tsx
@@ -15,7 +15,7 @@ export const ChatListItem: FC<ChatListItemProps> = (props) => {
         "gap-2 px-2 py-2 items-center",
         type == "odd" && "bg-[#E8ECEF] text-white",
         type == "even" && "bg-[#777D80]",
-        isLast && "rounded-b-xl"
+        isLast && "rounded-b-lg"
       )}
     >
       <FolderIcon
